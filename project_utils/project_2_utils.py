b9a7381b3bfd1cc073f51249ea2948b65e56ce3d
Cihan Soylu <cihansoylu@gmail.com>
2019-12-16 10:32:39 -0500

    Change validation ratio

--- project_utils/project_2_utils.py
+++ project_utils/project_2_utils.py
@@ -8,23 +8,23 @@ from sklearn.preprocessing import MinMaxScaler
 DATA_URL = "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data"
 
 def load_data():
-    
+
     file_path = tf.keras.utils.get_file("wdbc_data", DATA_URL)
 
     df = pd.read_csv(file_path, header = None)
     df = df.sample(frac=1, replace=True, random_state=1)
-    
+
     y = df[1].values
     x = df.drop([0, 1], axis = 1).values
-    
+
     y = np.array([int(s == 'M') for s in y])
     y = np.reshape(y, (-1,1))
-    
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, stratify = y, random_state=42)
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, stratify = y, random_state=1)
 
     scaler = MinMaxScaler()
     scaler.fit(x_train)
     x_train = scaler.transform(x_train)
     x_test = scaler.transform(x_test)
-    
+
     return x_train, x_test, y_train, y_test
