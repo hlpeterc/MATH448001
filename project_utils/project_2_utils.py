71996a12b88a8b5a694da5492f5b22b097142f94
cihan soylu <cihansoylu@cihans-MacBook-Pro.local>
2020-01-02 09:34:15 -0500

    Add import matplotlib

--- project_utils/project_2_utils.py
+++ project_utils/project_2_utils.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
