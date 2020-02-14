ea2066bc64e81822d6518a0b7669aa78c73670a4
Cihan Soylu <cihansoylu@gmail.com>
2020-02-14 09:19:12 -0500

    Add project 3 utils

---
+++ project_utils/project_3_utils.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def plot_decision_boundary(model):
+    w, b = model.trainable_variables
+
+    w = w.numpy()
+    b = b.numpy()
+
+    t = np.linspace(0.1,0.6,100)
+    plt.plot(t, -1/w[1]*(w[0]*t + b))
+
+def plot_data(x,y):
+    plt.scatter(x[:,0], x[:,1], c=y.squeeze())
